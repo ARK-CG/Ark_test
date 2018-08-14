c0843963b34b1a83993221fb1f4f7dd442060d23
ErgoFriend <ergoproxy2236@yahoo.co.jp>
2018-08-15 00:39:12 +0900

    as

--- homepage/ark_web/views.py
+++ homepage/ark_web/views.py
@@ -21,8 +21,15 @@ def gallery(request):
     return render(request, 'ark_web/gallery.html', content)
 
 
-def about(request):
-    content = {}
+def about(request,id):
+    work = Post.objects.get(id=id)
+    content = {
+        'title':work.title,
+        'context':work.body,
+        'date':work.pub_date,
+        'user':work.user,
+        'image':work.image,
+    }
     return render(request, 'ark_web/about.html', content)
 
 
@@ -39,5 +46,6 @@ def idea(request):
 
 
 def progress(request):
-    content = {}
+    idea = Idea.objects.order_by('-pub_date')
+    content = {'idea':idea}
     return render(request, 'ark_web/progress.html', content)
--- start-docker.sh
+++ start-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yes | docker network create --driver bridge shared
+yes | docker network create --driver bridge common_link
 cd nginx-proxy
 sudo docker-compose up -d
 cd ../homepage
